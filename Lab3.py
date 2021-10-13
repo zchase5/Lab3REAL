39b2388171008a0d2177a02b62f6a30de73e5d54
zchase5 <91498652+zchase5@users.noreply.github.com>
2021-10-13 02:17:22 +0000

    Trying to get code working without comp

---
+++ Lab3.py
@@ -0,0 +1,28 @@
+#   To check address: sudo i2cdetect -y 1
+
+import smbus
+
+class PCF8591:
+
+  def __init__(self,address):
+    self.bus = smbus.SMBus(1)
+    self.address = address
+
+  def read(self,chn): #channel
+      try:
+          self.bus.write_byte(self.address, 0x40 | chn)  # 01000000
+          self.bus.read_byte(self.address) # dummy read to start conversion
+      except Exception as e:
+          print ("Address: %s \n%s" % (self.address,e))
+      return self.bus.read_byte(self.address)
+
+  def write(self,val):
+      try:
+          self.bus.write_byte_data(self.address, 0x40, int(val))
+      except Exception as e:
+          print ("Error: Device address: 0x%2X \n%s" % (self.address,e))
+
+xValue=PCF8591(1)
+while True:
+  currentX=xValue.read()
+  xValue.write()
